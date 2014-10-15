500888fcb7957d64867b4d6696845f37f73ddd5f
dgtombs <cyan.spam@gmail.com>
2014-10-15 09:43:43 -0400

    Add script to create the DB schema.

---
+++ srcalc/src/main/resources/sql/create_schema.sql
@@ -0,0 +1,5 @@
+-- Creates the SRCalc schema.
+--
+-- For now, this is HSQLDB. We'll find out what our actual database is later.
+CREATE TABLE SPECIALTY(ID INTEGER NOT NULL PRIMARY KEY,NAME VARCHAR(255));
+
