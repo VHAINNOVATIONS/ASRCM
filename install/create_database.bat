840fda1f4c59a5825e3375f3d59056e6eed850fc
dgtombs <cyan.spam@gmail.com>
2014-12-10 12:45:03 -0500

    Move single-use commands to install.bat
    from create_database.bat. This permits using create_database.bat to reset an
    existing database without reconfiguring the MySQL or Glassfish servers.

--- install/create_database.bat
+++ install/create_database.bat
@@ -1,6 +1,6 @@
 @echo off
 
-REM Creates the srcalc database and Glassfish connectors to the database.
+REM Creates the srcalc database and populates it with some base data.
 
 SETLOCAL ENABLEEXTENSIONS
 
@@ -8,10 +8,6 @@ REM Load configuration settings.
 call config.bat
 
 REM Note: the 'root' user should have auto-login ability
-
-echo Creating 'srcalc' user...
-echo CREATE USER 'srcalc'@'localhost' IDENTIFIED BY '%SRCALC_PASSWORD%'; | %MYSQL% -u root
-IF ERRORLEVEL 1 goto Abort
 
 echo Creating 'srcalc' database...
 REM go into resources directory for reference to procedures_2013.csv
@@ -22,15 +18,6 @@ IF ERRORLEVEL 1 goto Abort
 
 echo Populating database with base data set...
 %MYSQL% -u root srcalc < ..\srcalc\src\main\resources\sql\insert_base_data.sql
-IF ERRORLEVEL 1 goto Abort
-
-echo Creating Connection Pool in Glassfish...
-REM Must use "call" because asadmin is a batch file.
-call %ASADMIN% create-jdbc-connection-pool --datasourceclassname com.mysql.jdbc.jdbc2.optional.MysqlDataSource --restype javax.sql.DataSource --validationtable=DUAL --isconnectvalidatereq=true --property password=%SRCALC_PASSWORD%:user=srcalc:url=jdbc\:mysql\://localhost\:3306/srcalc srcalcDbPool
-IF ERRORLEVEL 1 goto Abort
-call %ASADMIN% ping-connection-pool srcalcDbPool
-IF ERRORLEVEL 1 goto Abort
-call %ASADMIN% create-jdbc-resource --connectionpoolid srcalcDbPool jdbc/srcalcDB
 IF ERRORLEVEL 1 goto Abort
 
 goto End
