b8188990abff8202669ecd724dbd2c1dfec6b140
dgtombs <cyan.spam@gmail.com>
2015-01-05 15:48:15 -0500

    ASRC-84 prep: add a couple unit tests

--- srcalc/src/test/java/gov/va/med/srcalc/domain/variable/NumericalVariableTest.java
+++ srcalc/src/test/java/gov/va/med/srcalc/domain/variable/NumericalVariableTest.java
@@ -6,6 +6,13 @@ import org.junit.Test;
 
 public class NumericalVariableTest
 {
+    @Test(expected = NullPointerException.class)
+    public final void testSetDisplayNameNull()
+    {
+        final NumericalVariable var = SampleObjects.sampleAgeVariable();
+        var.setDisplayName(null);
+    }
+
     @Test(expected = IllegalArgumentException.class)
     public final void testSetDisplayNameTooLong()
     {
@@ -13,6 +20,13 @@ public class NumericalVariableTest
         var.setDisplayName(
                 // 81 characters
                 "01234567890123456789012345678901234567890123456789012345678901234567890123456789X");
+    }
+
+    @Test(expected = NullPointerException.class)
+    public final void testSetUnitsNull()
+    {
+        final NumericalVariable var = SampleObjects.sampleAgeVariable();
+        var.setUnits(null);
     }
 
     @Test(expected = IllegalArgumentException.class)
