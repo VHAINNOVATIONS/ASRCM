fba8302af0793699294bee386e5ccabae86ce06a
David Tombs <cyan.spam@gmail.com>
2015-07-01 13:33:15 -0400

    ASRC-53: fix an import

--- srcalc/src/main/java/gov/va/med/srcalc/domain/model/AbstractVariable.java
+++ srcalc/src/main/java/gov/va/med/srcalc/domain/model/AbstractVariable.java
@@ -8,9 +8,9 @@ import java.util.regex.Pattern;
 
 import javax.persistence.*;
 
-import com.github.rjeschke.txtmark.*;
 import org.hibernate.annotations.Proxy;
 
+import com.github.rjeschke.txtmark.Processor;
 import com.google.common.base.Optional;
 
 /**
